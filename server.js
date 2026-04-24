6d1be817fec411986d3460deefd27c0716deae43
shruti-jain07 <shrutijain19.2003@gmail.com>
2026-04-24 15:51:37 +0530

    chore: initial backend setup with express and mongodb connection

---
+++ server.js
@@ -0,0 +1,8 @@
+require("dotenv").config()
+const app=require("./src/app")
+const connectDB=require("./src/config/db")
+connectDB()
+const PORT=3000
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`)
+})
