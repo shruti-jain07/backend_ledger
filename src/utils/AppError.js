3e15c33a69a0075b42059073c348fa66830b6150
shruti-jain07 <shrutijain19.2003@gmail.com>
2026-04-28 13:59:12 +0530

    refactor:improved apperror

--- src/utils/AppError.js
+++ src/utils/AppError.js
@@ -1,9 +1,15 @@
+const ERROR_CODES=require("../constants/errorCodes")
 class AppError extends Error{
-    constructor(message,statusCode=500,code="SERVER_ERROR",details=[]){
-        super(message)
-        this.statusCode=statusCode
-        this.code=code
-        this.details=details
-    }
+    constructor(
+    message,
+    statusCode = 500,
+    code = ERROR_CODES.SERVER_ERROR,
+    details = []
+  ) {
+    super(message);
+    this.statusCode = statusCode;
+    this.code = code;
+    this.details = details;
+  }
 }
 module.exports=AppError
