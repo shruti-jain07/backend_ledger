7420ee8465508cceb4f8fc34a7eb06d4f50dd73a
shruti-jain07 <shrutijain19.2003@gmail.com>
2026-04-28 14:14:55 +0530

    refactor:combined log file for complete logging

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -8,7 +8,7 @@ const logger=createLogger({
     transports:[
         new transports.Console(),
         new transports.File({filename:"logs/error.log",level:"error"}),
-
+        new transports.File({ filename: "logs/combined.log" })
     ]
 })
 module.exports=logger
