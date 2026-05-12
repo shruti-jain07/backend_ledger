b198fd50aad3de46b7feecf12ebc3d1904cecf2d
shruti-jain07 <shrutijain19.2003@gmail.com>
2026-05-12 10:56:04 +0530

    Modified balance calculating functionality

--- src/models/account.model.js
+++ src/models/account.model.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose")
+const ledgerModel=require("./ledger.model")
 const accountSchema = new mongoose.Schema(
   {
     user: {
@@ -23,5 +24,50 @@ const accountSchema = new mongoose.Schema(
   },
 )
 accountSchema.index({user:1,status:1})//compound index
+accountSchema.methods.getBalance=async function(){
+  const balanceData=await ledgerModel.aggregate([
+        {
+            $match:{
+                account:this._id
+            }
+        },
+        {
+            $group:{
+                _id:null,
+                totalDebit:{
+                    $sum:{
+                        $cond:[
+                            {$eq:["$type","DEBIT"]},
+                            "$amount",
+                            0
+                        ]
+                    }
+                },
+                totalCredit:{
+                    $sum:{
+                        $cond:[
+                            {$eq:["$type","CREDIT"]},
+                            "$amount",
+                            0
+                        ]
+                    }
+                }
+            }
+        },
+        {
+            $project:{
+                _id:0,
+                balance:{
+                    $subtract:[
+                        "$totalCredit",
+                        "$totalDebit"
+                    ]
+                }
+            }
+        }
+    ])
+
+    return balanceData.length>0 ? balanceData[0].balance : 0
+}
 const accountModel=mongoose.model("account",accountSchema)
 module.exports=accountModel
