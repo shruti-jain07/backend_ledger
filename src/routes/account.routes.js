943c7277c59fa57b7c4d2a0206186669eec9bf47
shruti-jain07 <shrutijain19.2003@gmail.com>
2026-05-13 11:41:31 +0530

    Add account balance API

--- src/routes/account.routes.js
+++ src/routes/account.routes.js
@@ -3,8 +3,10 @@ const router=express.Router()
 
 const accountController=require("../controllers/account.controller")
 const {authMiddleware}=require("../middlewares/auth.middleware")
-
+//create account
 router.post("/",authMiddleware,accountController.createAccount)
+//get all account of logged in user
 router.get("/",authMiddleware,accountController.getAccounts)
-
+//get account balance of logged in user using accountId
+router.get("/:accountId/balance",authMiddleware,accountController.getAccountBalance)
 module.exports=router
